0671b2bf8d6462f35675f3183bb2bc31736caedc
AnfaalAbdulHai <133610398+AnfaalAbdulHai@users.noreply.github.com>
2024-06-28 22:28:05 +0500

    Add files via upload

---
+++ Assignment6/my-app/src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Quote</h1>
+        <p>The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela</p>
+        <p>It conveys a powerful message about resilience and perseverance. Here's an interpretation of its meaning:<br></br>
+
+Embracing Failure:
+
+The quote emphasizes that failure is a natural part of life. Everyone experiences setbacks and makes mistakes. It suggests that trying to avoid failure entirely is neither realistic nor the most admirable goal.<br></br>
+Value of Persistence:
+
+True success and glory come from the ability to recover and rise after facing challenges and failures. It's about how we respond to difficulties and our capacity to keep moving forward despite them.<br></br>
+Building Character:
+
+Overcoming obstacles and bouncing back from failures builds strength, character, and wisdom. Each time we rise after a fall, we grow stronger and more resilient.<br>
+Continuous Improvement:
+
+Life is a journey of continuous improvement. Falling and rising again is part of the learning process. Each fall teaches us something new, and each rise makes us more prepared for future challenges.
+Overall, the quote encourages us to view failures as opportunities for growth and to focus on our resilience and determination rather than being discouraged by our falls.</p>
+        <img src="https://cdn.britannica.com/67/75567-050-4EBBE84D/Nelson-Mandela.jpg" alt="Placeholder" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
